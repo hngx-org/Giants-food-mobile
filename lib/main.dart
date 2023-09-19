1793bdd8d7379123ae5f2e58a109676952fc6f46
Jamilu Salisu <jamilusalis@gmail.com>
2023-09-20 00:06:37 +0100

    withdraw lunch screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:giants_free_lunch/screens/home_screen.dart';
 import 'package:get/get.dart';
+import 'package:giants_free_lunch/screens/withdraw_lunch_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Free Lunch App',
-      theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Free Lunch App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const WithdrawLunchScreen(),
       ),
-      home: const HomePage(),
     );
   }
 }
-
---
+++ lib/themes/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color btnBgColor = Color.fromRGBO(21, 13, 87, 1);
+  static const Color btnTextColor = Color.fromARGB(203, 255, 137, 1);
+}
