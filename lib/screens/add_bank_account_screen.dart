0cb46fdfbabbb0517f4ad3b259c82e907ccb2aca
Paul Sola-Eniolawun <70896150+Musecreatives@users.noreply.github.com>
2023-09-20 09:20:21 +0100

    customNav logic issue

--- lib/screens/add_bank_account_screen.dart
+++ lib/screens/add_bank_account_screen.dart
@@ -1,28 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:giants_free_lunch/widgets/custom_bottom_bar.dart' as widget;
 
-class AddAccountController extends GetxController {
-  final TextEditingController accountNameController = TextEditingController();
-  final TextEditingController accountNumberController = TextEditingController();
+import '../controllers/add_bank_account_controller.dart' as addcontroller ;
 
-  // Add any other necessary logic for account creation here
 
-  @override
-  void onClose() {
-    // Clean up your controllers when the screen is closed to avoid memory leaks
-    accountNameController.dispose();
-    accountNumberController.dispose();
-    super.onClose();
-  }
-}
-class AddAccountScreen extends StatelessWidget {
-   AddAccountScreen({super.key});
+class AddBankAccountScreen extends StatelessWidget {
+  final addcontroller.AddBankAccountController _controller = Get.put(addcontroller.AddBankAccountController());
 
-  final AddAccountController controller = Get.put(AddAccountController());
-   
+  final Rx<widget.BottomBarItem> selectedItem;
+  
+   AddBankAccountScreen({super.key, required this.selectedItem});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Bank Account'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButtonFormField<String>(
+              value: _controller.selectedCountry,
+              onChanged: (newValue) {
+                _controller.updateSelectedCountry(newValue!);
+              },
+              items: _controller.countryList.map((String country) {
+                return DropdownMenuItem<String>(
+                  value: country,
+                  child: Text(country),
+                );
+              }).toList(),
+              decoration: const InputDecoration(labelText: 'Select Country'),
+            ),
+            const SizedBox(height: 20),
+            DropdownButtonFormField<String>(
+              value: _controller.selectedBank,
+              onChanged: (newValue) {
+                _controller.updateSelectedBank(newValue!);
+              },
+              items: _controller.bankList.map((String bank) {
+                return DropdownMenuItem<String>(
+                  value: bank,
+                  child: Text(bank),
+                );
+              }).toList(),
+              decoration: const InputDecoration(labelText: 'Select Bank'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _controller.accountNumberController,
+              decoration: const InputDecoration(labelText: 'Account Number'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                // Add logic to add bank account here
+                final result = await _controller.addBankAccount();
+
+                if (result) {
+                  // Show a success message using GetX SnackBar
+                  Get.snackbar(
+                    'Success',
+                    'Bank account added successfully',
+                    backgroundColor: Colors.green,
+                    snackPosition: SnackPosition.BOTTOM,
+                  );
+                } else {
+                  // Show an error message using GetX SnackBar
+                  Get.snackbar(
+                    'Error',
+                    'Failed to add bank account',
+                    backgroundColor: Colors.red,
+                    snackPosition: SnackPosition.BOTTOM,
+                  );
+                }
+              },
+              child: const Text('Add Bank Account'),
+            ),
+             widget.CustomBottomNavigationBar(selectedItem: _controller.selectedItem),
+          ],
+        ),
+      ),
+    );
   }
 }
-
