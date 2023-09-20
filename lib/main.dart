045c19945288b6ee3815619448535e746afac7a1
Paul Sola-Eniolawun <70896150+Musecreatives@users.noreply.github.com>
2023-09-20 12:31:58 +0100

    customButton resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './core/app_export.dart';
+import 'screens/add_bank_account_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: AddBankAccountScreen(),
     );
   }
 }
--- lib/screens/add_bank_account_screen.dart
+++ lib/screens/add_bank_account_screen.dart
@@ -1,86 +1,153 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../controllers/add_bank_account_controller.dart' as addcontroller ;
+import '../controllers/add_bank_account_controller.dart' as addcontroller;
 import '../widgets/custom_bottom_bar.dart';
-
+import '../core/app_export.dart';
 
 class AddBankAccountScreen extends StatelessWidget {
-  final addcontroller.AddBankAccountController _controller = Get.put(addcontroller.AddBankAccountController());
+  final addcontroller.AddBankAccountController _controller =
+      Get.put(addcontroller.AddBankAccountController());
 
-final Rx<BottomBarItem> selectedItem = BottomBarItem.Home.obs;
-  
-   AddBankAccountScreen({super.key});
+  final Rx<BottomBarItem> selectedItem = BottomBarItem.Home.obs;
+
+  AddBankAccountScreen({super.key});
+  // {
+  //   selectedItem.listen((item) {
+  //     switch (item) {
+  //       case BottomBarItem.Home:
+  //         Get.offAll(() => HomePage()); // Navigate to the Home page
+  //         break;
+  //       case BottomBarItem.Leaderboards:
+  //       //   Get.offAll(
+  //       //       () => LeaderBoard()); // Navigate to the Leaderboards screen
+  //         break;
+  //       case BottomBarItem.Profile:
+  //       //   Get.offAll(() => ProfilePage()); // Navigate to the Profile screen
+  //         break;
+  //     }
+  //   });
+  // };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Bank Account'),
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text(
+          'Add Bank Account',
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButtonFormField<String>(
-              value: _controller.selectedCountry,
-              onChanged: (newValue) {
-                _controller.updateSelectedCountry(newValue!);
-              },
-              items: _controller.countryList.map((String country) {
-                return DropdownMenuItem<String>(
-                  value: country,
-                  child: Text(country),
-                );
-              }).toList(),
-              decoration: const InputDecoration(labelText: 'Select Country'),
-            ),
-            const SizedBox(height: 20),
-            DropdownButtonFormField<String>(
-              value: _controller.selectedBank,
-              onChanged: (newValue) {
-                _controller.updateSelectedBank(newValue!);
-              },
-              items: _controller.bankList.map((String bank) {
-                return DropdownMenuItem<String>(
-                  value: bank,
-                  child: Text(bank),
-                );
-              }).toList(),
-              decoration: const InputDecoration(labelText: 'Select Bank'),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _controller.accountNumberController,
-              decoration: const InputDecoration(labelText: 'Account Number'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                // Add logic to add bank account here
-                final result = await _controller.addBankAccount();
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'Securely link your bank account',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 25),
+              DropdownButtonFormField<String>(
+                value: _controller.selectedCountry,
+                onChanged: (newValue) {
+                  _controller.updateSelectedCountry(newValue!);
+                },
+                items: _controller.countryList.map((String country) {
+                  return DropdownMenuItem<String>(
+                      value: country, child: Text(country));
+                }).toList(),
+                decoration: const InputDecoration(
+                  hintText: 'Select Country',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  prefixIcon: Icon(CupertinoIcons.globe),
+                ),
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField<String>(
+                value: _controller.selectedBank,
+                onChanged: (newValue) {
+                  _controller.updateSelectedBank(newValue!);
+                },
+                items: _controller.bankList.map((String bank) {
+                  return DropdownMenuItem<String>(
+                    value: bank,
+                    child: Text(bank),
+                  );
+                }).toList(),
+                decoration: const InputDecoration(
+                  hintText: 'Select Bank',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                  prefixIcon: Icon(CupertinoIcons.money_dollar_circle),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _controller.accountNumberController,
+                decoration: const InputDecoration(
+                  hintText: 'Account Number',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(CupertinoIcons.money_dollar),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  backgroundColor: const Color(0xFF150D57),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                onPressed: () async {
+                  // Add logic to add bank account here
+                  final result = await _controller.addBankAccount();
 
-                if (result) {
-                  // Show a success message using GetX SnackBar
-                  Get.snackbar(
-                    'Success',
-                    'Bank account added successfully',
-                    backgroundColor: Colors.green,
-                    snackPosition: SnackPosition.BOTTOM,
-                  );
-                } else {
-                  // Show an error message using GetX SnackBar
-                  Get.snackbar(
-                    'Error',
-                    'Failed to add bank account',
-                    backgroundColor: Colors.red,
-                    snackPosition: SnackPosition.BOTTOM,
-                  );
-                }
-              },
-              child: const Text('Add Bank Account'),
-            ),
-          ],
+                  if (result) {
+                    // Show a success message using GetX SnackBar
+                    Get.snackbar(
+                      'Success',
+                      'Bank account added successfully',
+                      backgroundColor: Colors.green,
+                      snackPosition: SnackPosition.BOTTOM,
+                    );
+                  } else {
+                    // Show an error message using GetX SnackBar
+                    Get.snackbar(
+                      'Error',
+                      'Failed to add bank account',
+                      backgroundColor: Colors.red,
+                      snackPosition: SnackPosition.BOTTOM,
+                    );
+                  }
+                },
+                child: const Text(
+                  'Add Account',
+                  style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
