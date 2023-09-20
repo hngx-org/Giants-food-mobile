1d500b84e1368bcf7276d5fadf2186824f65452b
stephen michael <chigomic99@gmail.com>
2023-09-20 20:19:38 +0100

    sign up screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:giants_free_lunch/screens/login_screen.dart';
 import './core/app_export.dart';
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SignUp(title: ''),
     );
   }
 }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -4,4 +4,5 @@ class AppTheme {
   Color appBackgroundColor = const Color(0xffEEEFF2);
   Color navBackgroundColor = const Color(0xFFEFECFF);
   Color primaryColor = const Color(0xFF150D57);
+  Color white = const Color(0xffffffff);
 }
