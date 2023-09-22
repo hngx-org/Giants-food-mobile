c4ff595674f5b43b7dfcc3f351a6beb711211683
1297820 <umunubo.lg@gmail.com>
2023-09-22 02:23:10 +0100

    edits

--- lib/controllers/invite_employee_controller.dart
+++ lib/controllers/invite_employee_controller.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class InviteController extends GetxController {
+  final formKey = GlobalKey<FormState>();
   final TextEditingController textController = TextEditingController();
 }
--- lib/screens/invite_employee.dart
+++ lib/screens/invite_employee.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:giants_free_lunch/controllers/invite_employee_controller.dart';
 import 'package:giants_free_lunch/core/app_export.dart';
-import 'package:giants_free_lunch/widgets/app_textField.dart';
+import 'package:giants_free_lunch/widgets/app_text_fields.dart';
 
 class InviteEmployee extends StatefulWidget {
   const InviteEmployee({super.key});
@@ -43,55 +43,55 @@ class _InviteEmployeeState extends State<InviteEmployee> {
           physics: const BouncingScrollPhysics(),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(20, 14, 20, 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 56,
-                ),
-                const Text(
-                  "Send an Invitation to an employee, they\nwill receive it in their email.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    height: 2,
+            child: Form(
+              key: _inviteController.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 56,
                   ),
-                ),
-                const SizedBox(
-                  height: 69,
-                ),
-                AppTextField(
-                  textTitle: "Email",
-                  textController: _inviteController.textController,
-                  hintText: "Email",
-                  // suffixIcon: IconButton(
-                  //   splashColor: Colors.transparent,
-                  //   icon: obscureText
-                  //       ? Icon(
-                  //           Icons.visibility_off,
-                  //           color: appTheme.black200,
-                  //         )
-                  //       : const Icon(
-                  //           Icons.visibility,
-                  //           color: Colors.deepPurple,
-                  //         ), //change icon based on boolean value
-                  //   onPressed: () {
-                  //     setState(() {
-                  //       obscureText = !obscureText;
-                  //     });
-                  //   },
-                  // ),
-                  // obscureText: obscureText,
-                ),
-                const SizedBox(
-                  height: 36,
-                ),
-                AppButton(
-                  buttonText: "Send Invitation",
-                  onPressed: () {},
-                ),
-              ],
+                  const Text(
+                    "Send an Invitation to an employee, they\nwill receive it in their email.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      height: 2,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 69,
+                  ),
+                  AppTextField(
+                    textTitle: "Email",
+                    textController: _inviteController.textController,
+                    hintText: "Email",
+                    validator: (value) {
+                      if (RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return null;
+                      } else {
+                        return "Please input a valid Email Address";
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 36,
+                  ),
+                  AppButton(
+                    buttonText: "Send Invitation",
+                    onPressed: () {
+                      if (_inviteController.formKey.currentState!.validate()) {
+                        print("valid");
+                      } else {
+                        print("Invalid");
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
