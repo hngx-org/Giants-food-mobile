04d375271633ed19f5eb7548d9000b10357e8b28
Paul Sola-Eniolawun <70896150+Musecreatives@users.noreply.github.com>
2023-09-22 23:45:16 +0100

    leaderboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:giants_free_lunch/screens/employee_sign_up_one_screen.dart';
 import 'package:giants_free_lunch/screens/login_screen.dart';
 import './core/app_export.dart';
+import 'screens/leader_board_screen.dart';
 
 AppTheme appTheme = AppTheme();
 void main() {
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-            home: const SignIn(),
+            home: LeaderBoard(),
           );
         });
   }
--- lib/screens/leader_board_screen.dart
+++ lib/screens/leader_board_screen.dart
@@ -26,14 +26,77 @@ class LeaderBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.dark,
     ));
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("LeaderBoard"),
+      appBar: AppBar(
+        backgroundColor: appTheme.appBackgroundColor,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+        centerTitle: true,
+        title: Text(
+          'Leaderboard',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: appTheme.primaryColor,
+            height: 2,
+          ),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(26.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Top 3 workers',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+                height: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage(imgUnsplashqayxtcv4aq31x31),
+              ),
+              title: Text(
+                'John Doe',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  height: 2,
+                ),
+              ),
+              trailing: Text(
+                '4.9',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
