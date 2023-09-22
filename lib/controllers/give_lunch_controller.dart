2bb5ca24219023b57dddeea164e69d6583bae0b1
Abaxx <abassife@gmail.com>
2023-09-22 06:47:31 +0100

    Add Getx, reduce button size and number of users display.

---
+++ lib/controllers/give_lunch_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class GiveLunchController extends GetxController {
+  final textEditController = TextEditingController();
+  var check = false.obs;
+
+}
